b5d874ec519430d831021172d5a9abc99fe69b39
Fantus <nikolay_tkachev_2001@mail.ru>
2025-05-28 21:03:39 +0300

    Объеденил ифы

--- core/src/main/java/com/mygame/Main.kt
+++ core/src/main/java/com/mygame/Main.kt
@@ -680,12 +680,7 @@ class CollisionManager(val modelInstance: com.badlogic.gdx.graphics.g3d.ModelIns
 
     private fun traverseSceneGraph(parentTransform: Matrix4, nodes: Iterable<Node>) {
         for (node in nodes) {
-            if (node.id.startsWith("anim_zone_", ignoreCase = false)) {
-                continue
-            }
-            if (node.id.startsWith("audio_zone_", ignoreCase = false)) {
-                continue
-            }
+            if (node.id.startsWith("anim_zone_") || node.id.startsWith("audio_zone_")) continue
             val worldTransform = Matrix4()
             worldTransform.set(parentTransform).mul(node.localTransform)
 
